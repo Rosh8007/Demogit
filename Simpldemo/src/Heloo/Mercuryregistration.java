51be9b7605a4e6fa28262ad0cab19f633b459eef
Rosh8007 <130126995+Rosh8007@users.noreply.github.com>
2023-04-17 09:18:42 +0530

    Update Mercuryregistration.java

--- Simpldemo/src/Heloo/Mercuryregistration.java
+++ Simpldemo/src/Heloo/Mercuryregistration.java
@@ -11,17 +11,14 @@ import org.testng.annotations.Test;
 import com.beust.jcommander.Parameter;
 
 public class Mercuryregistration {
-
+	
 	WebDriver driver;
-
 	@BeforeSuite
 	public void browsera() 
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Desktop\\Chromedriver\\chromedriver_win32\\chromedriver.exe");
 		driver=new ChromeDriver();	
 		driver.get("https://demo.guru99.com/test/newtours/register.php");
-
-
 	}
 	@BeforeTest
 	public void Bt() 
@@ -37,25 +34,14 @@ public class Mercuryregistration {
 		driver.findElement(By.name("lastName")).sendKeys(B);
 		driver.findElement(By.name("phone")).sendKeys(C);
 		driver.findElement(By.name("userName")).sendKeys(D);
-
 		driver.findElement(By.name("address1")).sendKeys(E);
-
 		driver.findElement(By.name("city")).sendKeys(F);
-
 		driver.findElement(By.name("state")).sendKeys(G);
-
 		driver.findElement(By.name("postalCode")).sendKeys(H);
-
-	
-
 		driver.findElement(By.name("email")).sendKeys(J);
 		driver.findElement(By.name("password")).sendKeys(K);
-
 		driver.findElement(By.name("confirmPassword")).sendKeys(L);
-
 		driver.findElement(By.name("submit")).click();
-                
-		
 		driver.close();
 		
 
